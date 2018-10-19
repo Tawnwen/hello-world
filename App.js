af9dc66d919085b06119b84fcdf4bb15d8a8b5b5
Xuan Chau <xuanchau98@gmail.com>
2018-10-18 20:02:54 -0400

    added states, buttons, etc.

--- App.js
+++ App.js
@@ -1,20 +1,40 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 export default class App extends React.Component {
+  state = { // a state is an object, used to change stuff around
+    colorOn: false, // state variable
+    hidden: false,
+    btnColor: false,
+    // for btn2
+    fancy: false,
+    text: false
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Text>Hello world! This is my first app!</Text>
-        <Text>I have no idea what these "text" labels are but they seem to print things</Text>
-        <Text>I suppose thats a start</Text>
-        <Text>Yikes, apparently apostrophes arent allowed here</Text>
-        <Text>And line spillage is a little wonky</Text>
-        <Text>But its OK. Baby steps. (why is Baby colored??)</Text>
+        <Text style={styles.text}>This text child inherits the text styling! Its red, larger font, and centered</Text>
+        <TouchableOpacity onPress={()=>{this.setState({colorOn: !this.state.colorOn, hidden: !this.state.hidden, btnColor: !this.state.btnColor})}}> // toggles colorOn true/false
+          <View style={{borderRadius:100, width: 100, height:100, backgroundColor: this.state.btnColor ? 'yellow' : '#ddd'}}>
+            <Text>PRESS ME</Text>
+          </View>
+        </TouchableOpacity>
+        <Text style={{fontSize: 40, color: this.state.colorOn ? 'green' : 'red'}}>I change color!</Text> // blue if true, else green
+        {this.state.hidden && <Text>I appear and disappear :)</Text>} // if hidden is true, this will show
+
+        <TouchableOpacity onPress={()=>{this.setState({fancy: !this.state.fancy, text: !this.state.text})}}>
+          <View style={[styles.btn2, {backgroundColor: this.state.fancy ? 'purple' : 'orange'}]}>
+            <Text style={{color: this.state.text ? 'white' : 'black'}}>ALSO PRESS ME</Text>
+          </View>
+        </TouchableOpacity>
       </View>
     );
   }
 }
+
+// text field
+// press submit button
+// to display text down here
 
 const styles = StyleSheet.create({
   container: {
@@ -22,5 +42,14 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    color: 'red',
+    fontSize: 20,
+    textAlign: 'center',
+  },
+  btn2: {
+    backgroundColor: 'black',
+    padding: 10,
   },
 });
